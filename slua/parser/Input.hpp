a5ea840a97ae686b121f0f53f26c102a99ee0d60
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2024-11-30 19:47:23 +0200

    comment & whitespace parsing code + more stuff added to input

--- slua/parser/Input.hpp
+++ slua/parser/Input.hpp
@@ -22,6 +22,43 @@ namespace sluaParse
 		std::span<const uint8_t> text;
 		size_t idx;
 
+		uint8_t peek()
+		{
+			if (idx >= text.size())
+				throw EndOfStreamError();
+
+			return text[idx];
+		}
+
+		//offset 0 is the same as peek()
+		uint8_t peekAt(const size_t offset)
+		{
+			if (idx + offset >= text.size()
+				|| idx > SIZE_MAX - offset)	//idx + offset overflows, so...
+				throw EndOfStreamError();
+
+			return text[idx + offset];
+		}
+
+		// span must be valid until next get(), so, any other peek()'s must not invalidate these!!!
+		std::span<const uint8_t> peek(const size_t count)
+		{
+			if (idx + count > text.size()	//position after this peek() can be at text.size(), but not above it
+				|| idx > SIZE_MAX - count)	//idx + count overflows, so...
+				throw EndOfStreamError();
+
+			std::span<const uint8_t> res = text.subspan(idx, count);
+
+			return res;
+		}
+		void skip(const size_t count = 1)
+		{
+			//if (idx >= text.size())
+			//	throw EndOfStreamError();
+
+			idx+= count;
+		}
+
 		uint8_t get()
 		{
 			if (idx >= text.size())
@@ -29,7 +66,7 @@ namespace sluaParse
 
 			return text[idx++];
 		}
-		// span must be valid, until next get()
+		// span must be valid until next get(), so, any peek()'s must not invalidate these!!!
 		std::span<const uint8_t> get(const size_t count)
 		{
 			if (idx + count > text.size()	//position after this get() can be at text.size(), but not above it
@@ -43,16 +80,26 @@ namespace sluaParse
 			return res;
 		}
 
+		/* Returns true, while stream still has stuff */
 		bool checkEndOfStream() const {
-			return idx >= text.size();
+			return idx < text.size();
 		}
 	};
 
 	//Here, so streamed inputs can be made
 	template<class T = Input>
 	concept AnyInput = requires(T t) {
+		{ t.skip() } -> std::same_as<void>;
+		{ t.skip((size_t)100) } -> std::same_as<void>;
+
 		{ t.get() } -> std::same_as<uint8_t>;
 		{ t.get((size_t)100) } -> std::same_as<std::span<const uint8_t>>;
+
+		{ t.peek() } -> std::same_as<uint8_t>;
+		{ t.peekAt((size_t)100) } -> std::same_as<uint8_t>;
+		{ t.peek((size_t)100) } -> std::same_as<std::span<const uint8_t>>;
+
+		/* Returns true, while stream still has stuff */
 		{ t.checkEndOfStream() } -> std::same_as<bool>;
 	};
 }
