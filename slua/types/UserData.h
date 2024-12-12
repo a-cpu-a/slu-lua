750c8c2981c4d46b4c5080222ea5722283d074e2
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2024-12-12 20:25:46 +0200

    change variable name

--- slua/types/UserData.h
+++ slua/types/UserData.h
@@ -10,20 +10,20 @@ namespace slua
 {
 	inline const size_t TYPE_ID_SIZE = 2;
 
+// After all typeids are initialized, this will contain a value that will always be more than any existing type id
+	inline uint16_t TYPE_ID_COUNT=0;
 
-	inline uint16_t _nextTypeId=0;
 	template<typename TYPE>
-	inline const uint16_t _typeId = _nextTypeId++;
+	inline const uint16_t _typeId = TYPE_ID_COUNT++;
 
 	template<typename TYPE>
 	inline uint16_t getTypeId() {
 		return _typeId<TYPE>;
 	}
-
-	//Changes the last 2 bytes into the types id
-	//Uses sizeof(PTR_T) to find where to change stuff
+	// Changes the last 2 bytes into the types id
+	// Uses sizeof(PTR_T) to find where to change stuff
 	template<typename TYPE,typename PTR_T>
-	inline void setUserTypeIdSeperate(PTR_T* ptr)
+	inline void setTypeIdSeperate(PTR_T* ptr)
 	{
 		uint8_t* dataPtr = reinterpret_cast<uint8_t*>(ptr);
 
@@ -32,10 +32,10 @@ namespace slua
 		dataPtr[sizeof(PTR_T)+0] = typeId;
 		dataPtr[sizeof(PTR_T)+1] = typeId>>8;
 	}
-	//Changes the last 2 bytes into the types id
-	//Uses sizeof(TYPE) to find where to change stuff
+	// Changes the last 2 bytes into the types id
+	// Uses sizeof(TYPE) to find where to change stuff
 	template<typename TYPE>
-	inline void setUserTypeId(TYPE* ptr) {
+	inline void setTypeId(TYPE* ptr) {
 		setUserTypeIdSeperate<TYPE,TYPE>(ptr);
 	}
 }
