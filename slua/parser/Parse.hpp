f7d6f233057acef819d93d78675fe40feb60f0d0
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-01-20 21:57:15 +0200

    remove some duplicate

--- slua/parser/Parse.hpp
+++ slua/parser/Parse.hpp
@@ -205,13 +205,8 @@ namespace sluaParse
 		 stat ::=  ‘;’ |
 		 varlist ‘=’ explist |
 		 functioncall |
-		 label |
-		 break |
-		 goto Name |
-		 do block end |
 		 while exp do block end |
 		 repeat block until exp |
-		 if exp then block {elseif exp then block} [else block] end |
 		 for Name ‘=’ exp ‘,’ exp [‘,’ exp] do block end |
 		 for namelist in explist do block end |
 		 function funcname funcbody |
