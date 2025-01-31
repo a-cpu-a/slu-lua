cf5363d4d32438c01d8ff84b4138d473fc223441
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-01-31 19:22:38 +0200

    remove duplicate ebnf stuff

--- slua/parser/adv/ReadNumeral.h
+++ slua/parser/adv/ReadNumeral.h
@@ -33,13 +33,13 @@ namespace sluaParse
 		HexSep		::= "x" | "X"
 		HexExpSep	::= "p" | "P"
 
-		DigList ::= Digit {Digit}
-		HexDigList ::= HexDigit {HexDigit}
+		DigList		::= Digit {Digit}
+		HexDigList	::= HexDigit {HexDigit}
 
 		NumSign ::= "+" | "-"
 
-		Digit ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-		HexDigit ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | a | b | c | d | e | f | A | B | C | D | E | F
+		Digit		::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+		HexDigit	::= Digit | a | b | c | d | e | f | A | B | C | D | E | F
 	*/
 
 	inline ExprData readNumeralExtra(AnyInput auto& in, const bool hex, const bool decPart, const char firstChar)
