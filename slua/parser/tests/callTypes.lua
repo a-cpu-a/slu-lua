0276d7947e639382981fea09f73781b82b3ffc0e
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-02-22 15:08:57 +0200

    new test + only restrict string after Name + update example to use the postfix macro invoke style. + file extension standard

---
+++ slua/parser/tests/callTypes.lua
@@ -0,0 +1,20 @@
+
+	(print)[=[Hello World!]=];
+	(print)[[Hello World!]];
+	(print)"Hello World!";
+	(print)'Hello World!'
+	
+	_G["print"][=[Hello World!]=]
+	_G["print"][[Hello World!]]
+	_G["print"]"Hello World!"
+	_G["print"]'Hello World!'
+	
+	print [=[Hello World!]=]
+	print [[Hello World!]]
+	print "Hello World!"
+	print 'Hello World!'
+	
+	print[=[Hello World!]=]
+	print[[Hello World!]]
+	print"Hello World!"
+	print'Hello World!'
