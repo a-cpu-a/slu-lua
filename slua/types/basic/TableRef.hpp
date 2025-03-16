76e1df671785768e990da2037ca01613ee02efdd
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-03-16 22:02:03 +0200

    remove lua state from table ref

--- slua/types/basic/TableRef.hpp
+++ slua/types/basic/TableRef.hpp
@@ -13,25 +13,24 @@ namespace slua
 {
 	struct TableRef
 	{
-		lua_State* L=nullptr;
 		int idx = 0;
 
 		constexpr TableRef() = default;
-		constexpr TableRef(lua_State* _L, const int _idx) :L(_L), idx(_idx) {}
+		constexpr TableRef(const int _idx) :idx(_idx) {}
 
 		//TODO: use [] operator instead
-		void setU8(const int64_t i, const uint8_t value)
+		void setU8(lua_State* L,const int64_t i, const uint8_t value)
 		{
 			lua_pushinteger(L, value);
 			lua_rawseti(L, idx, i);
 		}
-		size_t size() { return lua_rawlen(L, idx); }
-		void push_back(const uint8_t value) {
-			setU8(size() + 1, value);
+		size_t size(lua_State* L) { return lua_rawlen(L, idx); }
+		void push_back(lua_State* L, const uint8_t value) {
+			setU8(L,size(L) + 1, value);
 		}
 
 		static TableRef read(lua_State* L, const int idx) {
-			return TableRef(L, idx);
+			return TableRef(idx);
 		}
 		static bool check(lua_State* L, const int idx) {
 			return lua_istable(L, idx);
