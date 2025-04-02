0c417f99801a4e1eef9e6421386f5da3699a644d
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-04-02 21:53:01 +0300

    implement readErrType

--- slua/parser/adv/ReadType.hpp
+++ slua/parser/adv/ReadType.hpp
@@ -22,4 +22,44 @@ namespace sluaParse
 	{
 		return {};
 	}
+
+	/**
+	 * @brief 
+	 * @param res Output variable
+	 * @return true, if you need to return
+	 */
+	template<AnyInput In>
+	inline bool checkReadErrTypeQMark(In& in, ErrType& res)
+	{
+		if (in.peek() == '?')
+		{
+			res.isErr = true;
+			if (in.peekAt(1) == '?')
+			{// Its ?? or (type ??)
+				in.skip(2);
+				return true;
+			}
+
+			// Its (? type) or (type ? type)
+			in.skip();
+			res.err = readType();
+			return true;
+		}
+		return false;
+	}
+
+	template<AnyInput In>
+	inline ErrType readErrType(In& in)
+	{
+		ErrType res;
+
+		if (checkReadErrTypeQMark(in, res))
+			return res;
+
+		// Its (type) or (type ??) or (type ? type)
+		res.val = readType();
+
+		checkReadErrTypeQMark(in, res);
+		return res;
+	}
 }
