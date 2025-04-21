dca21978fbba946a24dc044cf3c5ddd1fd6ed9f3
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-04-21 21:11:49 +0300

    HashModPathView and EqualModPathView

--- slua/lang/BasicState.hpp
+++ slua/lang/BasicState.hpp
@@ -14,4 +14,29 @@ namespace slua::lang
 
 	using ModPath = std::vector<std::string>;
 	using ModPathView = std::span<const std::string>;
+
+	struct HashModPathView
+	{
+		using is_transparent = void;
+		constexpr std::size_t operator()(ModPathView data) {
+			std::size_t seed = data.size();  // Start with size to add some variation
+			std::hash<std::string> hasher;
+
+			for (const std::string& str : data)
+			{
+				seed ^= hasher(str) * 31 + (seed << 6) + (seed >> 2); // Someone, fix this lol
+			}
+
+			return seed;
+		}
+	};
+
+	struct EqualModPathView
+	{
+		using is_transparent = void;
+		constexpr bool operator()(ModPathView lhs, ModPathView rhs) {
+			return std::equal(begin(lhs), end(lhs),
+				begin(rhs), end(rhs));
+		}
+	};
 }
