7dd5a9b832a7e84311de97834f72c081fe4745eb
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-05-05 13:37:07 +0300

    lua version of operator utilities

--- slua/midlevel/Operator.hpp
+++ slua/midlevel/Operator.hpp
@@ -16,16 +16,19 @@ namespace slua::mlvl
 {
     enum class Assoc : uint8_t { LEFT, RIGHT };
 
+
+    template<bool isLua>
     constexpr uint8_t precedence(parse::BinOpType op) {
         switch (op)
         {
         case parse::BinOpType::EXPONENT: return 90;
 
+        case parse::BinOpType::MODULO: if constexpr (!isLua)return 70;
+
         case parse::BinOpType::DIVIDE:
         case parse::BinOpType::FLOOR_DIVIDE:
         case parse::BinOpType::MULTIPLY: return 80;
 
-        case parse::BinOpType::MODULO: return 70;
 
         case parse::BinOpType::SUBTRACT:
         case parse::BinOpType::ADD: return 60;
@@ -33,19 +36,21 @@ namespace slua::mlvl
         case parse::BinOpType::SHIFT_LEFT: 
         case parse::BinOpType::SHIFT_RIGHT: return 50;
 
-        case parse::BinOpType::BITWISE_OR:
-        case parse::BinOpType::BITWISE_XOR:
-        case parse::BinOpType::BITWISE_AND: return 40;
+        case parse::BinOpType::BITWISE_OR:if constexpr (isLua)return 40;
+        case parse::BinOpType::BITWISE_XOR:if constexpr (isLua)return 41;
+        case parse::BinOpType::BITWISE_AND: return 42;
 
-        case parse::BinOpType::RANGE_BETWEEN: return 30;
+        case parse::BinOpType::RANGE_BETWEEN: if constexpr (!isLua)return 30;
+            break;
 
-        case parse::BinOpType::CONCATENATE: return 20;
+        case parse::BinOpType::CONCATENATE:if constexpr (!isLua) return 20;
+            return 55;//Lua, between +- and << >>
 
 
         case parse::BinOpType::GREATER_EQUAL:
         case parse::BinOpType::GREATER_THAN:
         case parse::BinOpType::LESS_EQUAL:
-        case parse::BinOpType::LESS_THAN: return 11;
+        case parse::BinOpType::LESS_THAN: if constexpr(!isLua)return 11;
 
         case parse::BinOpType::EQUAL:
         case parse::BinOpType::NOT_EQUAL: return 10;
@@ -57,10 +62,14 @@ namespace slua::mlvl
         case parse::BinOpType::NONE:
             break;
         }
-        Slua_panic("Unknown operator, no precedence() defined");
+        Slua_panic("Unknown operator, no precedence<slua>() defined");
     }
 
+    template<bool isLua>
     constexpr Assoc associativity(parse::BinOpType op) {
+        if constexpr (isLua)
+            return op == parse::BinOpType::EXPONENT ? Assoc::RIGHT : Assoc::LEFT;
+
         switch (op)
         {
         case parse::BinOpType::EXPONENT:
@@ -74,16 +83,17 @@ namespace slua::mlvl
 
     // Returns the order of operations as indices into `extra`
     // Store parse::BinOpType in `extra[...].first`
-    std::vector<size_t> multiOpOrder(const auto& m) {
+    template<bool isLua>
+    constexpr std::vector<size_t> multiOpOrder(const auto& m) {
         std::vector<size_t> indices(m.extra.size());
         std::iota(indices.begin(), indices.end(), 0);
 
         std::sort(indices.begin(), indices.end(), [&](size_t a, size_t b) {
-            auto pa = precedence(m.extra[a].first);
-            auto pb = precedence(m.extra[b].first);
+            auto pa = precedence<isLua>(m.extra[a].first);
+            auto pb = precedence<isLua>(m.extra[b].first);
             if (pa != pb) return pa > pb;
 
-            Assoc assoc = associativity(m.extra[a].first); // same as b
+            Assoc assoc = associativity<isLua>(m.extra[a].first); // same as b
             if (assoc == Assoc::LEFT)
                 return a < b;
             else
